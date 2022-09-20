06b112e07cd9b3819ce2ce8b2713276a1ff2d256
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-09-20 10:26:07 -0700

    forumlate response solution

---
+++ Mod3-Workbook/Week8/Day2/solution/02-formulate-a-response-in-http/server.js
@@ -0,0 +1,31 @@
+// Your code here
+const http = require("http");
+
+const responseBody = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Hello there!</h1>
+    </body>
+    </html>
+    `;
+
+const server = http.createServer((req, res) => {
+  // server code goes here
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+
+    res.write(responseBody)
+    res.end()
+    return
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log("listening on port ", port));

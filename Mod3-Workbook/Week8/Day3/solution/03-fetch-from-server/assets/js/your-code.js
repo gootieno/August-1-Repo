0a674df65b273bebbb4fe5d7ce17b1935a24469c
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-09-21 11:16:12 -0700

    fetch from servere solution

--- Mod3-Workbook/Week8/Day3/solution/03-fetch-from-server/assets/js/your-code.js
+++ Mod3-Workbook/Week8/Day3/solution/03-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,76 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+  return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
+  return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here
+  // Your code here
+//   const url = "/dogs";
+
+//   const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+
+//   const body = new URLSearchParams({
+//     name: "Hachi Roku",
+//     age: 86,
+//   });
+
+//   const options = {
+//     method: "POST",
+//     headers: headers,
+//     body: body,
+//   };
+
+  // return fetch(url, options);
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      name: "Hachi Roku",
+      age: 86,
+    }),
+  });
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  // Your code here
+  const url = "/dogs";
+
+  const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+
+  /*
+    const variable1 = "yo"
+    const variable2 = "hi"
+    const object = {variable1, variable2}
+    object
+    { variable1: 'yo', variable2: 'hi' }
+  */
+  const body = new URLSearchParams({
+    name,
+    age,
+  });
+
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: body,
+  };
+
+  return fetch(url, options);
 }
 
 export function deleteDog(id) {
-      // Your code here
+  // Your code here
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: {
+      AUTH: "ckyut5wau0000jyv5bsrud90y",
+    },
+  });
 }

6db521a00800eb8b5c0370216decc788e0891be5
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-09-27 10:59:09 -0700

    create remove partial

--- Mod3-Workbook/Week9/Day2/solutions/03-create-remove-elements/create-remove.js
+++ Mod3-Workbook/Week9/Day2/solutions/03-create-remove-elements/create-remove.js
@@ -1,44 +1,49 @@
 /****************************** ADD DOG BUTTON ******************************/
 const add = document.getElementById("add");
 add.addEventListener("click", async () => {
-    try {
-        const res = await fetch("https://dog.ceo/api/breeds/image/random")
-        const data = await res.json();
+  try {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
 
-        const url = data.message; // URL of new dog image
+    const url = data.message; // URL of new dog image
+    console.log(url);
+    /*--------------- Get breed (Hint: Parse from URL) ---------------- */
+    // Your code here
 
-        /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here
+    /*------------ Create new dog card with the url above ------------- */
 
-        /*------------ Create new dog card with the url above ------------- */
-        /* (use the HTML structure for the current dog image in the index.html
+    // <li>
+    //     <figure>
+    //         <img src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg" />
+    //         <figcaption>hound-afghan</figcaption>
+    //     </figure>
+    // </li>
+
+    /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-        // Your code here
+    // Your code here
 
-        /* Add the new dog card as a child to the ul in the .gallery element */
-        // Your code here
-
-    } catch (e) {
-        console.log("Couldn't fetch dog :(")
-    }
+    /* Add the new dog card as a child to the ul in the .gallery element */
+    // Your code here
+  } catch (e) {
+    console.log("Couldn't fetch dog :(");
+  }
 });
 
 /************************** REMOVE FIRST DOG BUTTON **************************/
 const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
-    /*-------------------- Select the first dog card --------------------- */
-    // Your code here
-
-    /*-------------------- Remove the first dog card --------------------- */
-    // Your code here
+  /*-------------------- Select the first dog card --------------------- */
+  // Your code here
+  /*-------------------- Remove the first dog card --------------------- */
+  // Your code here
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
 const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
-    /*-------------------- Select the last dog card ----------------------- */
-    // Your code here
-
-    /*-------------------- Remove the last dog card ----------------------- */
-    // Your code here
+  /*-------------------- Select the last dog card ----------------------- */
+  // Your code here
+  /*-------------------- Remove the last dog card ----------------------- */
+  // Your code here
 });
